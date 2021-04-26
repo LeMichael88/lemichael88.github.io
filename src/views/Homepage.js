d400263ec1240790c43f9a26c2a8bb302591f9cf
Michael Le <le.michael88@gmail.com>
2021-04-25 22:02:12 -0400

    Add about section with profile picture

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Box, Center, Heading, VStack } from '@chakra-ui/react'
-// import BackgroundImage from '../resources/images/pattern.png'
+import { Box, Center, Heading, VStack, Image, HStack, Text } from '@chakra-ui/react'
+import ProfileImage from '../resources/images/MyPicture.JPG'
 import Particles from '../components/Particles'
 import Header from '../components/Header'
 
@@ -14,6 +14,21 @@ export default function Homepage() {
           <Heading size="xl">Hello! I&apos;m Michael.</Heading>
           <Heading size="md">Welcome to my website</Heading>
         </VStack>
+      </Center>
+      <Center px="20%">
+        <HStack>
+          <Image
+            pr="5%"
+            htmlWidth="318"
+            htmlHeight="358.5"
+            borderRadius="full"
+            src={ProfileImage}
+          />
+          <Text>
+            Hi! My name is Michael Le. I&apos;m currently in my second year of computer
+            science at the University of Waterloo.
+          </Text>
+        </HStack>
       </Center>
     </Box>
   )
