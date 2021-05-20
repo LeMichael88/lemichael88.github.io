fee735612451fbd3fa242130fb007fc9253aa0d6
Michael Le <le.michael88@gmail.com>
2021-05-19 22:09:49 -0400

    Create experience card

---
+++ src/components/ExperienceCard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  Flex,
+  Image,
+  Text,
+  UnorderedList,
+  ListItem,
+  VStack,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react'
+
+export default function ExperienceCard({ data }) {
+  const { company, location, role, time, description, image } = data
+  return (
+    <Wrap justify="center" pt="2.5rem">
+      <WrapItem>
+        <VStack spacing="0.5" fontSize="md">
+          <Image maxBlockSize="100px" src={image} />
+          <Text fontWeight="bold">{company}</Text>
+          <Text>{location}</Text>
+          <Text>{time}</Text>
+          <Text>{role}</Text>
+        </VStack>
+      </WrapItem>
+      <WrapItem>
+        <Flex>
+          <UnorderedList pl="2rem" maxW="350px">
+            {description.map((text) => (
+              <ListItem key={text[0]} wordBreak="break-word">
+                {text}
+              </ListItem>
+            ))}
+          </UnorderedList>
+        </Flex>
+      </WrapItem>
+    </Wrap>
+  )
+}
