be103b4cdb7b1417ed9220ad6e931b21920dee56
Michael Le <le.michael88@gmail.com>
2021-06-03 12:45:53 -0400

    Add everything to homepage

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,61 +1,94 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {
   Box,
+  Button,
   Center,
   Heading,
-  Stack,
+  Flex,
   VStack,
   Image,
   HStack,
+  Wrap,
+  WrapItem,
   Text,
 } from '@chakra-ui/react'
 import ProfileImage from '../resources/images/MyPicture.JPG'
 import Particles from '../components/Particles'
 import Header from '../components/Header'
 import Contact from '../components/Contact'
+import Experiences from '../components/Experiences'
+import ProjectGrid from '../components/ProjectGrid'
+import Footer from '../components/Footer'
 
 export default function Homepage() {
+  const AboutRef = useRef({})
+  const ContactRef = useRef({})
+  const ExpRef = useRef({})
+  const ProjRef = useRef({})
+
+  const AboutScroll = () => AboutRef.current.scrollIntoView({ behavior: 'smooth' })
+  const ContactScroll = () => ContactRef.current.scrollIntoView({ behavior: 'smooth' })
+  const ExpScroll = () => ExpRef.current.scrollIntoView({ behavior: 'smooth' })
+  const ProjScroll = () => ProjRef.current.scrollIntoView({ behavior: 'smooth' })
+  const scrolls = [AboutScroll, ContactScroll, ExpScroll, ProjScroll]
+
   return (
     <Box>
       <Particles />
-      <Header />
+      <Header callbacks={scrolls} />
 
       {/* Top Section */}
-      <Center padding="10em">
-        <VStack>
+      <Center py="7rem" px="10em">
+        <VStack textAlign="center">
           <Heading size="2xl">Hello! I&apos;m Michael.</Heading>
           <Heading size="lg">Welcome to my website</Heading>
         </VStack>
       </Center>
 
       {/* About Section */}
-      <Center px="20%">
-        <HStack>
-          <Image
-            pr="5%"
-            htmlWidth="318"
-            htmlHeight="358.5"
-            borderRadius="full"
-            src={ProfileImage}
-          />
-          <Stack>
-            <Text fontSize="lg">
-              Hi! My name is Michael Le and I&apos;m currently in my second year of
-              computer science at the University of Waterloo. I&apos;m a software
-              developer looking to learn as much as I can. I enjoy working cool projects
-              and solving programming problems too!
-            </Text>
-            <Text fontSize="lg">
-              I&apos;m based in Toronto, Canada and in my free time, I like to cook, bike,
-              and play basketball. Feel free to contact me or take a look at my resume
-              below!
-            </Text>
-          </Stack>
-        </HStack>
-      </Center>
+      <HStack ref={AboutRef} mx="20%">
+        <Wrap justify="center">
+          <WrapItem>
+            <Image
+              mb="3rem"
+              htmlWidth="318"
+              htmlHeight="358.5"
+              borderRadius="full"
+              src={ProfileImage}
+            />
+          </WrapItem>
+          <WrapItem>
+            <Flex textAlign="center">
+              <Text fontSize="lg">
+                Hi! My name is <b>Michael Le</b> and I&apos;m currently in my second year
+                of computer science at the <b>University of Waterloo</b>. I&apos;m a
+                software developer looking to learn as much as I can. I enjoy working cool
+                projects and solving programming problems too! I&apos;m based in Toronto,
+                Canada and in my free time, I like to cook, bike, and play basketball.
+                Feel free to contact me or take a look at my resume below!
+              </Text>
+            </Flex>
+          </WrapItem>
+        </Wrap>
+      </HStack>
 
-      {/* Contact Boxes */}
-      <Contact />
+      {/* Contact Section */}
+      <Box ref={ContactRef}>
+        <Contact />
+      </Box>
+
+      {/* Experiences Section */}
+      <Box ref={ExpRef}>
+        <Experiences />
+      </Box>
+
+      {/* Projects Section */}
+      <Box ref={ProjRef}>
+        <ProjectGrid />
+      </Box>
+
+      {/* Footer Section */}
+      <Footer />
     </Box>
   )
 }
